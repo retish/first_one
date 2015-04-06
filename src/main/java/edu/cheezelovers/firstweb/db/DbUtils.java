56c602c7ed8a96a6535f4099812fd11c0e5f1b02
kristina <svnkrist@gmail.com>
2015-04-07 01:36:45 +0300

    patches for maven and db utils

--- src/main/java/edu/cheezelovers/firstweb/db/DbUtils.java
+++ src/main/java/edu/cheezelovers/firstweb/db/DbUtils.java
@@ -15,14 +15,18 @@ public class DbUtils {
     public static Connection getConnection(){
         Connection connection = null;
         //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
-        String url = "jdbc:mysql://localhost:3306/blog";
+//        String url = "jdbc:mysql://localhost:3306/blog";
+        String url = "jdbc:postgresql://ec2-54-247-77-103.eu-west-1.compute.amazonaws.com:5432/d1cf17ka9duv7h?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
         //Имя пользователя БД
-        String name = "root";
+//        String name = "root";
+        String name = "vqlxdtlxbyirdr";
         //Пароль
-        String password = "placebo666";
+//        String password = "placebo666";
+        String password = "ZZrvouCqMB9pvoN3yfREC_DMqL";
         try {
             //Загружаем драйвер
-            Class.forName("com.mysql.jdbc.Driver");
+//            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("org.postgresql.Driver");
             System.out.println("Драйвер подключен");
             //Создаём соединение
             connection = DriverManager.getConnection(url, name, password);
